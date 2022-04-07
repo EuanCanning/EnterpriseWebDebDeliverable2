d8538b3575cea015a0caf0ad18e80fd5588c077e
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-07 14:05:56 +0100

    checking to see if it works without what i added for comments

--- server/express.js
+++ server/express.js
@@ -46,7 +46,7 @@ app.use(cors())
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use('/', authRoutes)
 app.use('/', userRoutes)
-app.use('/', commentsRoutes)
+//app.use('/', commentsRoutes)
 // proxy
 app.use('/api/dadjoke', proxy('https://icanhazdadjoke.com/'));
 
