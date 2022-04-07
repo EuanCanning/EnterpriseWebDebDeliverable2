300aec68936bcd62dc2a2fd98bb85e1fcccda15a
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-07 13:43:11 +0100

    maybe this will work

--- server/models/comments.model.js
+++ server/models/comments.model.js
@@ -16,15 +16,6 @@ const CommentSchema = new mongoose.Schema({
     type: String,
     trim: true,
     required: 'Comment is required'
-  },
-  likes: {
-    type: Number,
-    default: 0,
-    min: 0
-  },
-  created: {
-    type: Date,
-    default: Date.now
   }
 })
 
