e4877434f1fb843c3bd7950397bb279289e7a577
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-08 16:44:13 +0100

    rsvp model change

--- server/models/rsvp.model.js
+++ server/models/rsvp.model.js
@@ -14,6 +14,6 @@ const RsvpSchema = new mongoose.Schema({
 
 
 
-const rsvpModel = mongoose.model('Event', RsvpSchema);
+const rsvpModel = mongoose.model('Rsvp', RsvpSchema);
 rsvpModel.createIndexes();
 export default rsvpModel
