6e90444c06e3721a7e92ee7223ee8851c32bf9a8
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-09 22:21:27 +0100

    update comment fix?

--- client/comments/UpdateComment.js
+++ client/comments/UpdateComment.js
@@ -12,8 +12,29 @@ import auth from '../auth/auth-helper'
 import {update} from './api-comment.js'
 import {Redirect} from 'react-router-dom'
 import TextArea from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: theme.mixins.gutters({
+    padding: theme.spacing(1),
+    margin: theme.spacing(5)
+  }),
+  title: {
+    margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
+    color: theme.palette.openTitle
+  },
+  textArea: {
+    margin: 'auto',
+    height: 100,
+    width: 500
+  },
+  submit: {
+    margin: 'auto'
+  }
+}))
 
 export default function UpdateComment(props) {
+  const classes = useStyles()
   const [open, setOpen] = useState(false)
 
   const jwt = auth.isAuthenticated()
