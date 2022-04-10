caa86dcf2f4f7bc087f4c36c4eafe8be1f575bca
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-10 20:52:51 +0100

    Update Rsvp.js

--- client/events/Rsvp.js
+++ client/events/Rsvp.js
@@ -29,7 +29,7 @@ export default function Rsvp(props) {
   
   const classes = useStyles()
   const [values, setValues] = useState({
-    rsvp = ''
+    rsvp : ''
   })
 
   const handleChange = rsvp => event => {
