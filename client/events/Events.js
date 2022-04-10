d57eddc4c2d250e901c179da33a8516dc66bbae3
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-10 10:25:22 +0100

    created events js

---
+++ client/events/Events.js
@@ -0,0 +1,108 @@
+import React, {useState, useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Typography from '@material-ui/core/Typography'
+import ArrowForward from '@material-ui/icons/ArrowForward'
+import Person from '@material-ui/icons/Person'
+import {Link} from 'react-router-dom'
+import {list,userRsvps} from './api-event.js'
+import auth from '../auth/auth-helper'
+//import AddComment from './AddComment.js'
+import Edit from '@material-ui/icons/Edit' 
+//import DeleteComment from './DeleteComment.js'
+//import UpdateComment from './UpdateComment.js'
+ 
+const useStyles = makeStyles(theme => ({
+  root: theme.mixins.gutters({
+    padding: theme.spacing(1),
+    margin: theme.spacing(5)
+  }),
+  title: {
+    margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
+    color: theme.palette.openTitle
+  }
+}))
+
+export default function Events() {
+  const classes = useStyles()
+  const [comments, setComments] = useState([])
+  const [myrsvps, setMyrsvps] = useState([])
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+    const jwt = auth.isAuthenticated()
+
+    list({t: jwt.token}, signal).then((data) => {
+      if (data && data.error) {
+        console.log(data.error)
+      } else {
+        setComments(data)
+      }
+    })
+
+    userRsvps({
+      userId: auth.isAuthenticated().user._id 
+    },{t: jwt.token}, signal).then((data) => {
+      if (data && data.error) {
+        console.log(data.error)
+      } else {
+        setMycomments(data)
+      }
+    })
+
+    return function cleanup(){
+      abortController.abort()
+    }
+  }, [])
+
+
+    return (
+      <div>
+      <Paper className={classes.root} elevation={4}>
+        <Typography variant="h6" className={classes.title}>
+          Comments
+        </Typography>
+        <List>
+         {comments.map((item, i) => {
+          return <ListItem>
+                  <Link to={"/comments/" + item._id} key={i}>
+                  <ListItem Button> 
+                  <ListItemText primary={item.name} secondary={item.comment}/>
+                  </ListItem> 
+                  </Link>
+                  <ListItemSecondaryAction>
+                  {
+                      
+                        mycomments.map((myitem, i) => {
+                          if (myitem._id==item._id){
+                            return <div>
+                              <UpdateComment commentId={item._id} comment={item.comment}/>
+                              <DeleteComment commentId={item._id}/>
+                        </div>}
+                        }
+                      )
+                      
+                    }
+                  </ListItemSecondaryAction>
+                </ListItem>
+                    
+                 
+               })
+             }
+        </List>
+      </Paper>
+      
+      {/* <AddComment/> ╌> */}
+      </div>
+    )
+}
