bc9000c73052a82a5dd11cb0c116413cb7b41560
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-12 11:06:47 +0100

    update delete fix

--- client/comments/DeleteComment.js
+++ client/comments/DeleteComment.js
@@ -26,7 +26,7 @@ export default function DeleteComment(props) {
       if (data && data.error) {
         console.log(data.error)
       } else {
-        location.reload()
+        window.location.href = '/comment/' + props.commentId
       }
     })
   }
