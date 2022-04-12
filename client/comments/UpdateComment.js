d3525bb94b810d6141ab684ae43f1aa6251de63c
EuanCanning <47995736+EuanAndrewCanning@users.noreply.github.com>
2022-04-12 10:44:39 +0100

    reverting change

--- client/comments/UpdateComment.js
+++ client/comments/UpdateComment.js
@@ -15,10 +15,10 @@ import TextArea from '@material-ui/core/TextField'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
-  normal: {
+  root: theme.mixins.gutters({
     padding: theme.spacing(1),
     margin: theme.spacing(5)
-  },
+  }),
   title: {
     margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
     color: theme.palette.openTitle
